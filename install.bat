4f664cb8ccac5bce96064128780be7d6b0182c52
Arthur730 <134970703+Arthur730@users.noreply.github.com>
2024-11-26 21:52:58 +0300

    update instaler

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-xcopy /s /e nim\target\release\nim.exe "C:\Program Files\NimeScript\nim.exe"
+xcopy /s /e nim\release\nim.exe "C:\Program Files\NimeScript\nim.exe"
 
 setx PATH "%PATH%;C:\Program Files\NimeScript"
 
